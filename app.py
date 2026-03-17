494f2e8e5fe6b4f045d78655526e7ed0c82e17aa
esmattt77 <esmatt88@gmail.com>
2026-03-17 03:46:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
-# app.py
-from bot import main
+import os
+import asyncio
+from flask import Flask, request
+from aiogram import types
+from bot import bot, dp
+
+app = Flask(__name__)
+
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+PORT = int(os.getenv("PORT", 10000))
+
+@app.route("/", methods=["GET", "HEAD"])
+async def index():
+    return "Bot is running..."
+
+@app.route("/", methods=["POST"])
+async def webhook():
+    update = types.Update.model_validate(request.json, context={"bot": bot})
+    await dp.feed_update(bot, update)
+    return "OK"
+
+async def setup_webhook():
+    webhook_path = f"{WEBHOOK_URL}"
+    await bot.set_webhook(webhook_path)
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(setup_webhook())
+    app.run(host="0.0.0.0", port=PORT)
