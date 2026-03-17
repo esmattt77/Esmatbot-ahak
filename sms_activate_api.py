7d45786495285aa4aa61c53eefdc94d7caa79e27
esmattt77 <esmatt88@gmail.com>
2026-03-18 01:37:08 +0300

    Refactor API calls to use _call method

--- sms_activate_api.py
+++ sms_activate_api.py
@@ -6,40 +6,50 @@ class HeroSMSAPI:
         self.api_key = api_key
         self.url = "https://hero-sms.com/stubs/handler_api.php"
 
-    def _get(self, action, params=None):
-        query_params = {'api_key': self.api_key, 'action': action}
-        if params: query_params.update(params)
+    def _call(self, action, params=None):
+        p = {'api_key': self.api_key, 'action': action}
+        if params: p.update(params)
         try:
-            response = requests.get(self.url, params=query_params, timeout=20)
-            return response.text
-        except: return "ERROR"
+            r = requests.get(self.url, params=p, timeout=25)
+            return r.text
+        except: return "ERROR_CONNECTION"
 
     def get_balance(self):
-        res = self._get('getBalance')
+        res = self._call('getBalance')
         return res.split(":")[1] if "ACCESS_BALANCE" in res else "0"
 
     def get_number(self, service, country=0):
-        # استخدام getNumberV2 للحصول على بيانات JSON أدق كما في التوثيق
-        res = self._get('getNumberV2', {'service': service, 'country': country})
-        try:
-            return json.loads(res)
-        except:
-            # fallback للنظام القديم إذا فشل JSON
-            return res
+        # طلب الرقم بنظام V2 للحصول على JSON كامل
+        res = self._call('getNumberV2', {'service': service, 'country': country})
+        try: return json.loads(res)
+        except: return res
 
-    def get_prices(self, service):
-        res = self._get('getPrices', {'service': service})
-        try:
-            data = json.loads(res)
-            # التوثيق يظهر أن الرد قد يكون مصفوفة [ { "service": { "cost": x } } ]
-            # أو قاموس { "country": { "service": { "cost": x } } }
-            return data
+    def get_prices(self, service=None, country=None):
+        params = {}
+        if service: params['service'] = service
+        if country: params['country'] = country
+        res = self._call('getPrices', params)
+        try: return json.loads(res)
         except: return {}
 
-    def set_status(self, activation_id, status):
-        # 6 للإنهاء، 8 للإلغاء
-        return self._get('setStatus', {'id': activation_id, 'status': status})
+    def get_history(self):
+        res = self._call('getHistory')
+        try: return json.loads(res)
+        except: return []
 
-    def get_status(self, activation_id):
-        return self._get('getStatus', {'id': activation_id})
+    def get_services_list(self):
+        res = self._call('getServicesList', {'lang': 'en'})
+        try: return json.loads(res)
+        except: return {}
+
+    def get_top_countries(self, service):
+        res = self._call('getTopCountriesByService', {'service': service})
+        try: return json.loads(res)
+        except: return []
+
+    def set_status(self, aid, status):
+        return self._call('setStatus', {'id': aid, 'status': status})
+
+    def get_status(self, aid):
+        return self._call('getStatus', {'id': aid})
         
